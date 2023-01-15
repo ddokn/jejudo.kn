11d3983ff4d60f19168389d22cbbf29781d0922c
ddokn <36984568+ddokn@users.noreply.github.com>
2023-01-15 14:11:41 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   publicPath: '/'
-// })
-module.exports = {
-  outputDir: "./docs",
-  publicPath: "./"
-}
-=======
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/'
 })
->>>>>>> dk-home
